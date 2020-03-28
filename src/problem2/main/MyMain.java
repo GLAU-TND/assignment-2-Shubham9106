25b6ad64e546ef9438c6ad229fda385bfa1d9cd4
Shubham9106 <shubham.sharma_cs18@gla.ac.in>
2020-03-28 12:56:01 +0530

    Pre Traverse Post Traverse Class

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -8,4 +8,27 @@ package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    package problem02;
+    public class PrePostTraversecheckMain {
+
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            PrePostTraversecheck ob=new PrePostTraversecheck();
+            ob.insert(40);
+            ob.insert(50);
+            ob.insert(30);
+            ob.insert(35);
+            ob.insert(100);
+            ob.insert(90);
+            ob.insert(12);
+            ob.insert(34);
+            System.out.println("PRE ORDER TRAVERSING");
+            ob.traversePreOrder(ob.getRoot());
+            System.out.println();
+            System.out.println("POST ORDER TRAVERSING");
+            ob.traversePostOrder(ob.getRoot());
+            System.out.println("so both condtion are satisfy");
+        }
+
+    }
 }
